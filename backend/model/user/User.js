e81ab86098a9df6ff63fade4e82e9d016cda5ef4
Bahadır Kalaçay <36983849+bahadirkalacay@users.noreply.github.com>
2022-11-13 12:34:53 +0300

    Update User.js

--- backend/model/user/User.js
+++ backend/model/user/User.js
@@ -15,7 +15,7 @@ const userSchema = new mongoose.Schema(
     profilePhoto: {
       type: String,
       default:
-        "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png",
+        "https://picsum.photos/200/300",
     },
     email: {
       type: String,
